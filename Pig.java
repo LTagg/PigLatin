42ef2c98435f792270350bd585029c77e9d82631
LTagg <LTagg@users.noreply.github.com>
2016-11-19 22:08:40 -0500

    Update Pig.java

--- Pig.java
+++ Pig.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 public class Pig {
-
-	public static void main(String[] args)
-	{
-		System.out.println("Enter sentence or word: ");
-		Scanner scanner = new Scanner(System.in);
-
-		String firstVowelThing = scanner.nextLine();
-		{
-			String first = firstVowelThing.substring(0,1);
-			String slice = firstVowelThing.substring(1,firstVowelThing.length());
-			System.out.println(slice + first + "ay");
-			scanner.close();
+	public static void main(String[] args) {
+		Scanner input = new Scanner( System.in );
+		String SE ="";
+		String[] v;
+		System.out.print("Type your freaking words ");
+		SE = input.nextLine();//Advances this scanner, returns the skipped input
+		v = SE.split(" "); //divides sentence into multiple words
+		for (String word : v) {
+			if (word.startsWith("aeiouAEIOU")){
+				System.out.print(word + " ay "); }
+			else 
+				System.out.print(word.substring(1)+word.substring(0,1)+"ay ");
+			input.close();
 		}
 	}
-}	
+}
